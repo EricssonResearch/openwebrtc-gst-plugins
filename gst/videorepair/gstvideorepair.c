6e9da996a7685691bf4fc502fb10f9b22ecabdf2
Arun Raghavan <arun@centricular.com>
2014-10-08 22:25:13 +0530

    videorepair: Register callback function names for debug

--- gst/videorepair/gstvideorepair.c
+++ gst/videorepair/gstvideorepair.c
@@ -102,8 +102,8 @@ static void gst_videorepair_class_init(GstVideoRepairClass *klass)
         "Requests intra pictures on GAP events and (optionally) drops buffers until they arrive",
         "Ericsson AB, http://www.ericsson.com");
 
-    gobject_class->set_property = gst_videorepair_set_property;
-    gobject_class->get_property = gst_videorepair_get_property;
+    gobject_class->set_property = GST_DEBUG_FUNCPTR(gst_videorepair_set_property);
+    gobject_class->get_property = GST_DEBUG_FUNCPTR(gst_videorepair_get_property);
 
     obj_properties[PROP_DROP_UNTIL_INTRA] = g_param_spec_boolean("drop-until-intra",
         "Drop until intra",
